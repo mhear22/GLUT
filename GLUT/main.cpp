f41a582caf8dc7a4472b354504e5e8c9c0e1941a
McKay Heares <rosanneheares@rosannes-macbook-pro.local>
2016-05-13 00:56:09 +1000

    added GLFW headers

--- GLUT/main.cpp
+++ GLUT/main.cpp
@@ -1,16 +1,41 @@
 //
 //  main.cpp
-//  GLUT
+//  OpenGL-Mac
 //
-//  Created by Rosanne Heares on 13/05/2016.
-//
+//  Created by Rosanne Heares on 12/05/2016.
+//  Copyright © 2016 Rosanne Heares. All rights reserved.
 //
 
 #include <iostream>
+#include <thread>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
-    return 0;
+#ifdef __APPLE__
+#include <GLFW/glfw3.h>
+#else
+#include <GL/glfw.h>
+#endif
+
+int main()
+{
+	glfwInit();
 	
+	GLFWwindow* w = glfwCreateWindow(400, 400, "", NULL, NULL);
+	glfwMakeContextCurrent(w);
+	while (!glfwWindowShouldClose(w))
+	{
+		
+		
+		glBegin(GL_TRIANGLES);
+		
+		glVertex3f(1,1,0);
+		glVertex3f(0,1,0);
+		glVertex3f(-1,0,0);
+		
+		glEnd();
+		
+		glfwSwapBuffers(w);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
 }
