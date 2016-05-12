fe78e1c47669ca4b15038394750464829ddc6254
Rosanne Heares <rosanneheares@rosannes-macbook-pro.local>
2016-05-13 00:41:54 +1000

    Initial Commit

---
+++ GLUT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLUT
+//
+//  Created by Rosanne Heares on 13/05/2016.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
