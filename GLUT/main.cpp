f8ff7dcb8ccce19930f41f0317662a136ecb2dc1
McKay Heares <rosanneheares@rosannes-macbook-pro.local>
2016-05-14 13:19:07 +1000

    init openGL

--- GLUT/main.cpp
+++ GLUT/main.cpp
@@ -17,13 +17,17 @@
 
 int main()
 {
-	glfwInit();
+	if(!glfwInit())
+	{
+		exit(0);
+	}
 	
 	GLFWwindow* w = glfwCreateWindow(400, 400, "", NULL, NULL);
 	glfwMakeContextCurrent(w);
 	while (!glfwWindowShouldClose(w))
 	{
-		glClear(0);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		
 		glBegin(GL_TRIANGLES);
 		
 		glVertex3f(1,-1,0);
