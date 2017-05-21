ec5079228eeab59d7efaa4487be3be0cddabbc61
McKay <mhear22@gmail.com>
2017-05-21 11:37:16 +1000

    inital C# commit

---
+++ OpenGL/Program.cs
@@ -0,0 +1,55 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using System;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
+
+namespace ConsoleApp1
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			using(var game = new Game())
+			{
+				game.Run(144.0);
+			}
+		}
+	}
+
+	public class Game : GameWindow
+	{
+		public Game()
+			: base(1920, 1080, GraphicsMode.Default, "", GameWindowFlags.FixedWindow)
+		{
+			
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			GL.ClearColor(0, 0, 0, 0);
+			GL.Enable(EnableCap.DepthTest);
+		}
+
+		protected override void OnKeyUp(KeyboardKeyEventArgs e)
+		{
+			base.OnKeyUp(e);
+
+			if(e.Key == Key.Escape)
+			{
+				Exit();
+			}
+		}
+
+		protected override void OnRenderFrame(FrameEventArgs e)
+		{
+			base.OnRenderFrame(e);
+
+			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+			
+
+			SwapBuffers();
+		}
+	}
+}
